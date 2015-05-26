7e1b1d5d63be9e458895b1e78ab7a11f27f295b8
Ian Carreon <ian.carreon@parametrichuman.org>
2015-05-26 18:28:05 -0400

    first commit

--- rbac/views.py
+++ rbac/views.py
@@ -7,4 +7,15 @@ def home(request):
 
 @login_required
 def dashboard(request):
-	return render_to_response('dashboard.html', {}, context_instance=RequestContext(request))
+    user_id = request.user.id
+    object_type = int(request.GET.get('object_type', 1))
+    
+    print 'user_id = request.user.id is ', user_id
+    print 'object_type ', object_type
+    
+    
+    #option = int(request.GET.get('object', 0))
+    #return render_to_response('dashboard.html', {}, context_instance=RequestContext(request))
+    return render_to_response('dashboard.html', {'object_type':object_type}, context_instance=RequestContext(request))
+
+
