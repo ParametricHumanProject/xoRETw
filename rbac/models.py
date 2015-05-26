973255aa87ce76ef09ef2cf01e5faf018d20b6f9
Ian Carreon <ian.carreon@parametrichuman.org>
2015-05-25 23:54:21 -0400

    first commit

--- rbac/models.py
+++ rbac/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+class Step(models.Model):
+    name = models.CharField(max_length=50)
+    step = models.ManyToManyField('self')
+
+class Condition(models.Model):
+    name = models.CharField(max_length=50)
+
+class Scenario(models.Model):
+    name = models.CharField(max_length=50)
+    step = models.ManyToManyField(Step)
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    scenario = models.ManyToManyField(Scenario)
+
+class Objective(models.Model):
+    name = models.CharField(max_length=50)
+    step = models.ManyToManyField(Step)
+    objective = models.ManyToManyField('self')
+    scenario = models.ManyToManyField(Scenario)
+    
+class Obstacle(models.Model):
+    name = models.CharField(max_length=50)
+    objective = models.ManyToManyField(Objective)
+
+class ContextConstraint(models.Model):
+    name = models.CharField(max_length=50)
+    condition = models.ManyToManyField(Condition)
+    obstacle = models.ManyToManyField(Obstacle)
+    objective = models.ManyToManyField(Objective)
+
+class WorkProfile(models.Model):
+    name = models.CharField(max_length=50)
+    task = models.ManyToManyField(Task)
+    
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+    role = models.ManyToManyField('self')
+
+class Permission(models.Model):
+    name = models.CharField(max_length=50)
+    step = models.OneToOneField(Step)
+    role = models.ManyToManyField(Role)
+    context_constraint = models.ManyToManyField(ContextConstraint)
+    
+
--- xoRETw/settings.py
+++ xoRETw/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration'
+    'registration',
+    'rbac',
 )
 
 MIDDLEWARE_CLASSES = (
