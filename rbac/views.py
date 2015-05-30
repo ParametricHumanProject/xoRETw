2603f5f25f2e8ac74936a399b81600550e0e64b9
Ian Carreon <ian.carreon@parametrichuman.org>
2015-05-30 04:50:00 -0400

    interim backup

--- rbac/views.py
+++ rbac/views.py
@@ -14,24 +14,22 @@ def dashboard(request):
 
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        print 'request is ', request
-        name = request.POST.get('name', None)
-        #conditions = request.POST.get('conditions[]', None)
+        objective_name = request.POST.get('name', None)
+        objective_type = request.POST.get('type', None)
         conditions = request.POST.getlist('conditions[]', None)
-        print 'name is ', name
-        print 'conditions is ', conditions
+        
+        print 'objective_name is ', objective_name
+        print 'objective_type is ', objective_type
+        print 'conditions are ', conditions
         
         response = json.dumps('{"message":"Ok"}')
+        
         return HttpResponse(response, content_type='application/json')
     # if a GET (or any other method) we'll create a blank form
     else:
         print 'user_id = request.user.id is ', user_id
         print 'object_type ', object_type
         
-        
-        #option = int(request.GET.get('object', 0))
-        #return render_to_response('dashboard.html', {}, context_instance=RequestContext(request))
-    
     return render_to_response('dashboard.html', {'object_type':object_type}, context_instance=RequestContext(request))
 
 
